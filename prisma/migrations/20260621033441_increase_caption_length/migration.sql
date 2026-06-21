5058ad9fb22ca293fccc6d7cb6cb59c773b1c87a
farhan <mofarhannn@gmail.com>
2026-06-21 12:14:25 +0700

    feat(feed-controller): implement post updates, increase caption capacity to text, and add request validation with Zod

---
+++ prisma/migrations/20260621033441_increase_caption_length/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Post" ALTER COLUMN "caption" SET DATA TYPE VARCHAR(1500);
--- routes/feed.route.js
+++ routes/feed.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { AuthMiddleware } from '../middleware/auth.middleware.js';
 import upload from '../middleware/upload.middleware.js';
-import { CreatePost, GetFeed, GetDetailPost, DeletePost } from '../controllers/feed.controller.js';
+import { CreatePost, GetFeed, GetDetailPost, DeletePost, UpdatePost } from '../controllers/feed.controller.js';
 
 const FeedRouter = express.Router();
 FeedRouter.post("/", AuthMiddleware, upload.single("image"), CreatePost);
+FeedRouter.put("/:postId", AuthMiddleware, upload.single("image"), UpdatePost);
 FeedRouter.get("/", AuthMiddleware, GetFeed);
 FeedRouter.get("/:postId", AuthMiddleware, GetDetailPost);
 FeedRouter.delete("/:postId", AuthMiddleware, DeletePost);
